4f5c9edc06133946a6d8260e1043c83907806512
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2011-06-04 15:37:23 -0300

    removed ugly if/elif block from distpatch.helpers, using a mapping instead

--- distpatch/helpers.py
+++ distpatch/helpers.py
@@ -5,18 +5,18 @@ import os
 def uncompressed_filename_and_compressor(tarball):
     '''returns the filename of the given tarball uncompressed and the compressor.
     '''
+    
+    compressors = {
+        '.gz': ('gzip', ''),
+        '.bz2': ('bzip2', ''),
+        '.xz': ('xz', ''),
+        '.lzma': ('lzma', ''),
+        '.tgz': ('gzip', '.tar'),
+        '.tbz2': ('bzip2', '.tar'),
+    }
+    
     dest, ext = os.path.splitext(tarball)
-    compressor = None
-    if ext in ('.gz', '.tgz'):
-        compressor = 'gzip'
-    elif ext in ('.bz2', '.tbz2'):
-        compressor = 'bzip2'
-    elif ext == '.xz':
-        compressor = 'xz'
-    elif ext == '.lzma':
-        compressor = 'lzma'
-    else:
-        dest += ext
-    if ext in ('.tgz', '.tbz2'):
-        return dest + '.tar', compressor
-    return dest, compressor
+    compressor = compressors.get(ext.lower(), None)
+    if compressor is None:
+        return tarball, None
+    return dest + compressor[1], compressor[0]
