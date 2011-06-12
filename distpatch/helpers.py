4e8dc70047761ef16ce2136f94e1fbf40735fb5c
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2011-06-12 03:47:58 -0300

    added a distpatch.stats module.

--- distpatch/helpers.py
+++ distpatch/helpers.py
@@ -21,3 +21,24 @@ def uncompressed_filename_and_compressor(tarball):
     if compressor is None:
         return tarball, None
     return dest + compressor[1], compressor[0]
+
+
+def format_size(size):
+
+    KB = 1024
+    MB = KB * 1024
+    GB = MB * 1024
+    TB = GB * 1024
+
+    size = float(size)
+
+    if size > TB:
+        return '%.3f TB' % (size / TB)
+    elif size > GB:
+        return '%.3f GB' % (size / GB)
+    elif size > MB:
+        return '%.3f MB' % (size / MB)
+    elif size > KB:
+        return '%.3f KB' % (size / KB)
+    else:
+        return '%.0f B' % size
---
+++ distpatch/stats.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+from matplotlib import pyplot
+
+from distpatch.deltadb import DeltaDB
+from distpatch.helpers import format_size
+
+
+class Stats:
+
+    def __init__(self, deltadb_file):
+        self.deltadb = sorted(DeltaDB(deltadb_file), self._cmp)
+
+    def _cmp(self, x, y):
+            x_ = int(x.dest.chksums['size'])
+            y_ = int(y.dest.chksums['size'])
+            if x_ < y_:
+                return -1
+            elif x_ > y_:
+                return 1
+            return 0
+
+    def downloaded_size(self):
+        rd = {}
+        for record in self.deltadb:
+            dest = int(record.dest.chksums['size'])
+            delta = int(record.delta.chksums['size'])
+            ratio = float(delta) / dest
+            rd[record.delta.fname] = {
+                'dest': format_size(dest),
+                'delta': format_size(delta),
+                'ratio': ratio,
+            }
+        return rd
+
+    def average_economy_graph(self):
+        ratio = []
+        for record in self.deltadb:
+            delta = float(record.delta.chksums['size'])
+            dest = float(record.dest.chksums['size'])
+            ratio.append(100 - ((delta / dest) * 100))
+        pyplot.plot(range(1, len(ratio) + 1), sorted(ratio))
+        pyplot.axes()
+        pyplot.xlabel('Deltas (total: %i)' % len(ratio))
+        pyplot.ylabel('Percentage of savings (for compressed files)')
+        pyplot.show()
+
+    def downloaded_size_graph(self):
+        dest = []
+        delta = []
+        for record in self.deltadb:
+            dest.append(int(record.dest.chksums['size']))
+            delta.append(int(record.delta.chksums['size']))
+        pyplot.plot(range(len(dest)), dest)
+        pyplot.plot(range(len(delta)), delta)
+        pyplot.show()
